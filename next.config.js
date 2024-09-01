fd0554a853e70ec74a4d70296d935cff614a821b
Shreyas Kumar <94731699+shkshreyas@users.noreply.github.com>
2024-09-01 11:14:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  output: "export",
-  reactStrictMode: true,
+  // This setting is optional for static exports
   trailingSlash: true,
-}
+  // Ensure your configuration is valid
+  // Check Next.js documentation for valid configurations
+};
